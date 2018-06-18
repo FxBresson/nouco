1b8d253b276f040da1d791194238f11d1f8ba5b9
FxBresson <fx-bresson@live.fr>
2018-06-18 14:40:12 +0200

    Home Structure #1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,88 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  sections: Array<{}>;
+
+
   constructor(public navCtrl: NavController) {
+
+    this.sections = [
+      {
+        sectionId: 'services',
+        sectionTitle: 'Trouver un service',
+        nbCards: 2,
+        cards: [
+          {
+            cardId: 'service-1',
+            title: 'Service 1',
+            description: 'Description du service 1',
+            img: ''
+          },
+          {
+            cardId: 'service-2',
+            title: 'Service 2',
+            description: 'Description du service 2',
+            img: ''
+          },
+          {
+            cardId: 'service-3',
+            title: 'Service 3',
+            description: 'Description du service 3',
+            img: ''
+          }
+        ]
+      },
+      {
+        sectionId: 'news',
+        sectionTitle: 'Les nouveautés',
+        nbCards: 1,
+        cards: [
+          {
+            cardId: 'service-1',
+            title: 'Service 1',
+            description: 'Description du service 1',
+            img: ''
+          },
+          {
+            cardId: 'service-2',
+            title: 'Service 2',
+            description: 'Description du service 2',
+            img: ''
+          },
+          {
+            cardId: 'service-3',
+            title: 'Service 3',
+            description: 'Description du service 3',
+            img: ''
+          }
+        ]
+      },
+      {
+        sectionId: 'workshops',
+        sectionTitle: 'Les Ateliers',
+        nbCards: 2,
+        cards: [
+          {
+            cardId: 'service-1',
+            title: 'Service 1',
+            description: 'Description du service 1',
+            img: ''
+          },
+          {
+            cardId: 'service-2',
+            title: 'Service 2',
+            description: 'Description du service 2',
+            img: ''
+          },
+          {
+            cardId: 'service-3',
+            title: 'Service 3',
+            description: 'Description du service 3',
+            img: ''
+          }
+        ]
+      },
+    ]
 
   }
 
