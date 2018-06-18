531bb147c831c91b5a7db2d29771eb35e1632f04
FxBresson <fx-bresson@live.fr>
2018-06-18 09:40:37 +0200

    Add SearchBar Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AboutPage } from '../pages/about/about';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { SearchBarComponent } from '../components/search-bar/search-bar';
 
 @NgModule({
   declarations: [
