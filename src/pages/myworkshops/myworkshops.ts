2f47a5ce32e377e9799a6e4925469d147cbd3e04
FxBresson <fx-bresson@live.fr>
2018-06-18 14:41:36 +0200

    MyWorkshops Structure #1

--- src/pages/myworkshops/myworkshops.ts
+++ src/pages/myworkshops/myworkshops.ts
@@ -7,8 +7,52 @@ import { NavController } from 'ionic-angular';
 })
 export class MyWorkshopsPage {
 
+  pastWS: Array<{}>
+  futurWS: Array<{}>
+  showPast: Boolean
+
   constructor(public navCtrl: NavController) {
 
+    this.showPast = true;
+
+    this.pastWS = [
+      {
+        cardId: 'service-1',
+        cat: 'Catégorie',
+        title: 'Service 1 - PAST',
+        description: 'Description du service 1',
+        img: ''
+      },
+      {
+        cardId: 'service-2',
+        cat: 'Catégorie',        
+        title: 'Service 2 - PAST',
+        description: 'Description du service 2',
+        img: ''
+      }
+    ]
+
+    this.futurWS = [
+      {
+        cardId: 'service-1',
+        cat: 'Catégorie',
+        title: 'Service 1 - FUTUR',
+        description: 'Description du service 1',
+        img: ''
+      },
+      {
+        cardId: 'service-2',
+        cat: 'Catégorie',        
+        title: 'Service 2 - FUTUR',
+        description: 'Description du service 2',
+        img: ''
+      }
+    ]
+  }
+
+  showPastWS(bool) {
+    this.showPast = bool;
+    console.log(this.showPast)
   }
 
 }
